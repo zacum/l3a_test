9d3bf4c17f12b22d22e3eb148a4325b37c7a3a23
zacum <mfilgyhty@hotmail.com>
2023-01-18 07:09:58 +0900

    dev: add DropDownContents

---
+++ src/components/DropDownContents.ts
@@ -0,0 +1,217 @@
+export interface IDropDownOption {
+  disabled: boolean,
+  icon: string,
+  title: string,
+  description: string,
+  url: string
+}
+
+export const aboutOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/lamp.png",
+    title: "Vision",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/paper.webp",
+    title: "Research",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/tag.webp",
+    title: "Docs",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/pen.webp",
+    title: "Updates",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/bag.webp",
+    title: "Career",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+export const productsOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/wheel1.webp",
+    title: "Streaming Service",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/dice.webp",
+    title: "Historical-data-as-a-service",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/wheel.webp",
+    title: "Query Engine",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/common.webp",
+    title: "Easily Deploy Data Applications",
+    description: "Developers to rapidly build, deploy, and manage",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/layer.webp",
+    title: "Decentralized Data Stack Nodes",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+export const technologyOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/cloud.webp",
+    title: "Unified API",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/wheel1.webp",
+    title: "Universal Data Collector (UDC)",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/circles.webp",
+    title: "Decentralized Data Mesh (DDM)",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/chain.webp",
+    title: "Tamper-proof ETL",
+    description: "Developers to rapidly build, deploy, and manage",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/dots.webp",
+    title: "OpenMetrics",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+export const infrastructureOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/chain.webp",
+    title: "Exchange Connectors",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/super.webp",
+    title: "SuperNode",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: true,
+    icon: "icons/node.webp",
+    title: "Node",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+export const transparencyOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/layer.webp",
+    title: "Open Infrastructure",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/github.webp",
+    title: "Fully open source",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/wheel.webp",
+    title: "Open R&D",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/note.webp",
+    title: "Realtime operation",
+    description: "Developers to rapidly build, deploy, and manage",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/dollar.webp",
+    title: "Open Audits",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+export const communityOptions: IDropDownOption[] = [
+  {
+    disabled: false,
+    icon: "icons/lamp.png",
+    title: "Vision",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/earth.webp",
+    title: "Join Community",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/man.webp",
+    title: "Become a verified Contributor",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/cup.webp",
+    title: "Community Grants",
+    description: "Developers to rapidly build, deploy, and manage",
+    url: "",
+  },
+  {
+    disabled: false,
+    icon: "icons/calendar.webp",
+    title: "Career",
+    description: "End users can subscribe to live data feeds for free, i.e., Coinbase BTC/USD",
+    url: "",
+  },
+]
+
